6e7dd401cf114f810bc15177a3ca0c2d53cb29c8
Chihiro298 <129707380+Chihiro298@users.noreply.github.com>
2025-07-08 18:04:25 +0900

    20250708_smallbugfix

--- crp/pre/prep.sh
+++ crp/pre/prep.sh
@@ -8,7 +8,7 @@
 L=259200
 XY="720 360"
 L2X=../../map/dat/l2x_l2y_/l2x.hlf.txt
-L2Y=../../map/dat/l2x_l2y_/l2x.hlf.txt
+L2Y=../../map/dat/l2x_l2y_/l2y.hlf.txt
 LONLAT="-180 180 -90 90"
 SUF=.hlf
 
@@ -19,7 +19,7 @@ SUF=.hlf
 #L=11088
 #XY="84 132"
 #L2X=../../map/dat/l2x_l2y_/l2x.ko5.txt
-#L2Y=../../map/dat/l2x_l2y_/l2x.ko5.txt
+#L2Y=../../map/dat/l2x_l2y_/l2y.ko5.txt
 #LONLAT="124 131 33 44"
 #SUF=.ko5
 
@@ -27,7 +27,7 @@ SUF=.hlf
 #L=32400
 #XY="180 180"
 #L2X=../../map/dat/l2x_l2y_/l2x.ks1.txt
-#L2Y=../../map/dat/l2x_l2y_/l2x.ks1.txt
+#L2Y=../../map/dat/l2x_l2y_/l2y.ks1.txt
 #LONLAT="129 132 31 34"
 #SUF=.ks1
 
